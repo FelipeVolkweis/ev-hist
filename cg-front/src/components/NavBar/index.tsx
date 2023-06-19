80d0a7efae20c575c13b2bcdd2de3180fc247443
Felipe Volkweis <felipezvolk@gmail.com>
2023-06-19 12:20:47 -0300

    navbar allinclusion icon

--- cg-front/src/components/NavBar/index.tsx
+++ cg-front/src/components/NavBar/index.tsx
@@ -9,7 +9,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import AllInclusiveIcon from '@mui/icons-material/AllInclusive';
 
 interface NavPage {
     page: string,
@@ -49,7 +49,7 @@ function ResponsiveAppBar() {
     <AppBar position="static" sx={{background: "#0439FF"}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <AllInclusiveIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -106,7 +106,7 @@ function ResponsiveAppBar() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <AllInclusiveIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
